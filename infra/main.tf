1fc5df43f8b93f6d07c830619c9b8d3b700e6f32
Dylan Armstrong <dylan.r.armstrong@outlook.com>
2023-06-19 22:27:45 +1000

    First commit with S3 bucket

---
+++ infra/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+resource "aws_s3_bucket" "web_bucket" {
+  bucket = "dylanarmstrong-net-web"
+
+  tags = {
+    Name = "dylanarmstrong-net-web"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "web_bucket_versioning" {
+  bucket = aws_s3_bucket.web_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "web_bucket_website_config" {
+  bucket = aws_s3_bucket.web_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
